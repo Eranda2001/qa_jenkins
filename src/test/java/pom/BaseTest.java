c7125a033170800091efce7fdf4d097e19d4d22d
Liviru Eranda Weerasinghe <121021896+Eranda2001@users.noreply.github.com>
2025-01-19 13:00:28 +0530

    Update BaseTest.java

--- src/test/java/pom/BaseTest.java
+++ src/test/java/pom/BaseTest.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 
 public class BaseTest {
 
